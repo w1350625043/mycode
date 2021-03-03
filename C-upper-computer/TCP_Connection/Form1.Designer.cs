c962bc4d179f9aaa5e0e3f5f846d62fe7a1a7b7a
王聪昊 <1350625043@qq.com>
2021-03-03 09:45:39 +0800

    C-TCP-connect-upper

---
+++ C-upper-computer/TCP_Connection/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace TCP_Connection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(633, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "连接";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "IP地址";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(416, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "端口号";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(489, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 25);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AllowDrop = true;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(140, 50);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(176, 23);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.DropDown += new System.EventHandler(this.comboBox1_DropDown);
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged_1);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(78, 250);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(101, 19);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(78, 144);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(600, 100);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(78, 292);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(600, 76);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(684, 312);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(81, 56);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "send";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(280, 394);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(188, 44);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Clear RecvBuff";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(78, 394);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(156, 44);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Clear SendBuff";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ C-upper-computer/TCP_Connection/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+namespace TCP_Connection
+{
+    public partial class Form1 : Form
+    {
+        private TcpClient myTcpClient = null;
+        
+        Thread ConnectThread;
+
+        string ipAddress;
+        int Port = 0;
+
+        string path = null;
+
+        private NetworkStream networkStream = null;
+        private Thread ReceiveThread;
+
+        public Form1()
+        {
+            InitializeComponent();
+          
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void ConnectMethod()
+        {
+            myTcpClient = new TcpClient();
+            try
+            {
+                myTcpClient.Connect(ipAddress, Port);
+
+                networkStream = myTcpClient.GetStream();
+                ReceiveThread = new Thread(ReceiveDataMethod);
+
+                ReceiveThread.Start();
+                try
+                {
+                    networkStream.Write(new byte[] { 0x31, 0x32, 0x33 }, 0, 3);
+                }
+                catch 
+                {
+                
+                }
+                Invoke((new Action(() =>
+                {
+                    button1.Text = "断开";
+                })));
+
+            }
+            catch (Exception)
+            {
+                Invoke((new Action(() =>
+                {
+                    button1.Text = "连接";
+                })));
+                try { myTcpClient.Close(); } catch { }
+            }
+        }
+        private void getIPAddress()
+        {
+            IPAddress[] hostipspool = Dns.GetHostAddresses("");
+            comboBox1.Items.Clear();
+            foreach (IPAddress ipa in hostipspool)
+            {
+                if (ipa.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    comboBox1.Items.Add(ipa.ToString());
+                    comboBox1.SelectedIndex = comboBox1.Items.Count > 0 ? 0 : -1;
+                }
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "连接")
+            {
+                ipAddress = comboBox1.Text.ToString();
+                Port = Convert.ToInt32(textBox1.Text.ToString());
+                ConnectThread = new Thread(ConnectMethod);
+                ConnectThread.Start();
+
+                StreamWriter sw = new StreamWriter(path, true);
+
+                if (!(comboBox1.Items.Contains(comboBox1.Text)))
+                {
+
+                    sw.WriteLine(comboBox1.Text);
+
+                }
+                comboBox1.Items.Insert(0, comboBox1.Text);
+
+                sw.Close();
+            }
+            else
+            {
+                try { myTcpClient.Close(); } catch { }
+                Invoke((new Action(() =>
+                {
+                    button1.Text = "连接";
+                })));
+            }
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_DropDown(object sender, EventArgs e)
+        {
+            getIPAddress();
+            path = @"d:1.ini";
+            if (File.Exists(path))
+            {
+                StreamReader sr = new StreamReader(path, true);
+                while (sr.Peek() > 0)
+                {
+                    comboBox1.Items.Add(sr.ReadLine());
+                }
+                sr.Close();
+                comboBox1.Text = (string)comboBox1.Items[0];
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            getIPAddress();
+        }
+        private string GetTimeStr()
+        {
+            /* 获取当前时间字符串函数 */
+            string tempTimeStr = DateTime.Now.Year.ToString() + "-" + DateTime.Now.Month.ToString().PadLeft(2, '0')
+                        + "-" + DateTime.Now.Day.ToString().PadLeft(2, '0') + "  " + DateTime.Now.Hour.ToString().PadLeft(2, '0')
+                        + ":" + DateTime.Now.Minute.ToString().PadLeft(2, '0') + ":" + DateTime.Now.Second.ToString().PadLeft(2, '0')
+                        + ":" + DateTime.Now.Millisecond.ToString().PadLeft(2, '0'); /* 以年-月-日时分秒的格式命名文件 */
+            return tempTimeStr;
+        }
+
+        private void ReceiveDataMethod()
+        {
+            int RecvCnt = 0;
+            byte[] recvBytes = new byte[1024];
+            while (true)
+            {
+                try
+                {   if (myTcpClient.Client.Poll(20, SelectMode.SelectRead) && (myTcpClient.Client.Available == 0))
+                    {
+                        myTcpClient.Close();
+                    }
+                    RecvCnt = networkStream.Read(recvBytes,0,recvBytes.Length);
+                    Invoke((new Action(() =>
+                        {
+                            DateTime t = System.DateTime.Now;
+                          
+                            textBox2.AppendText(GetTimeStr());
+                            textBox2.AppendText("\r\n");
+                            textBox2.AppendText(new ASCIIEncoding().GetString(recvBytes,0,RecvCnt));
+                        textBox2.AppendText("\r\n");
+                        })));
+                }
+                catch (Exception ex)
+                {
+                    Invoke((new Action(() =>
+                    {
+                        button1.Text = "连接";
+                    })));
+                    try { ReceiveThread.Abort(); }
+                    catch { }
+                    try { networkStream.Dispose(); }
+                    catch { }
+                    try { myTcpClient.Close(); }
+                    catch { }
+                }
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            try { ReceiveThread.Abort(); } catch { }
+            try { networkStream.Dispose(); } catch { }
+            try { myTcpClient.Close(); } catch { }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            byte[] sendbyte = Encoding.Default.GetBytes(textBox3.Text.ToString());
+            if (sendbyte.Length > 0)
+            {
+                try { networkStream.Write(sendbyte, 0, sendbyte.Length); }
+                catch (Exception) { MessageBox.Show("请检查连接","提示！"); }
+            }
+            else
+            { MessageBox.Show("数据不能为空","提示"); }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox3.Clear();
+        }
+
+        private void comboBox2_DropDown(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
